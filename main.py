4e3f295d89134b6028e0abf2b809c9161839da1f
blackkong8 <blackkong8@gmail.com>
2022-10-01 13:17:33 +0000

    simple tui application

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
-def __name__ == '__main__':
-    print("Hello, world!")
+import time
+
+import pytermgui as ptg
+
+
+def macro_time(fmt: str) -> str:
+    return time.strftime(fmt)
+
+
+ptg.tim.define("!time", macro_time)
+
+with ptg.WindowManager() as manager:
+    manager.layout.add_slot("Body")
+    manager.add(
+        ptg.Window(
+            "[bold]The current time is:[/]\n\n[!time 75]%c", box="EMPTY")
+    )
