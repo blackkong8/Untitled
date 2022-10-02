dc55208785befa630944ce7b1238b484c27e321e
blackkong8 <blackkong8@gmail.com>
2022-10-02 03:43:09 +0000

    change placeholder layout

--- textual/test.py
+++ textual/test.py
@@ -5,8 +5,8 @@ from textual.widgets import Placeholder
 class SimpleApp(App):
 
     async def on_mount(self) -> None:
-        await self.view.dock(Placeholder(), edge="left", size=40)
-        await self.view.dock(Placeholder(), Placeholder(), edge="top")
+        await self.view.dock(Placeholder(name="hi1"), Placeholder(name="hi2"), edge="left", size=20)
+        await self.view.dock(Placeholder(), Placeholder(), Placeholder(), edge="top")
 
 
 SimpleApp.run(log="textual.log")
